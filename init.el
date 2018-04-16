d7e65b20ec92f7c416831668100caf919db3c134
SXH <shenxuhui1992@gmail.com>
2018-04-17 00:32:21 +0800

    quick fix c-mode auto newline

--- init.el
+++ init.el
@@ -303,6 +303,8 @@ layers configuration.
   This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;;; quick fix
+  (remove-hook 'c-mode-common-hook 'spacemacs//c-toggle-auto-newline)
   ;;; emacs base setting begin
   (global-unset-key (kbd "C-x m"))
   (global-set-key (kbd "C-SPC") nil)
