9e1e4cfc2a79658c986d7c570ec4f5f01157bbe4
SXH <shenxuhui1992@gmail.com>
2019-04-03 10:39:55 +0800

    update

--- init.el
+++ init.el
@@ -17,16 +17,19 @@ values."
    ;; List of configuration layers to load. If it is the symbol `all' instead
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
-   '(octave
+   '(
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      ;; spacemacs base
+     octave
      theming
      helm
      better-defaults
+     ;; ivy
+     treemacs
      docker
      (ibuffer :variables ibuffer-group-buffers-by 'projects)
      (shell :variables
@@ -81,7 +84,9 @@ values."
    dotspacemacs-additional-packages '(wttrin
                                       switch-window
                                       youdao-dictionary
-                                      keyfreq)
+                                      keyfreq
+                                      sublimity
+                                      doom-themes)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '(magithub)
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
@@ -138,7 +143,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(doom-opera
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state nil
@@ -288,7 +293,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
            ;; cursor
            (cursor :background "DarkGoldenrod2")
            ;; state color
-           (spacemacs-emacs-face :foreground "black" :background "DarkGoldenrod2") ;; Purple1
+           (spacemacs-emacs-face :foreground "black" :background "DarkGoldenrod2") ;; Purple1,
            ))
         )
   (setq configuration-layer--elpa-archives
@@ -322,7 +327,6 @@ you should place your code here."
   (global-set-key (kbd "C-c n") 'git-gutter+-next-hunk)
   (global-set-key (kbd "C-x M-m") 'shell)
   (global-set-key (kbd "C-x n e") 'flycheck-next-error)
-  (global-set-key (kbd "C-x g") 'spacemacs/toggle-golden-ratio)
   (global-set-key (kbd "C-x 5 n") 'make-frame)
   (setq undo-tree-visualizer-diff nil)
   (setq dotspacemacs-which-key-delay 1.7)
@@ -353,8 +357,8 @@ you should place your code here."
   ;;; whitespace mode setting begin
   (setq whitespace-line-column 80)
   (setq whitespace-style '(face lines-tail))
-  (add-hook 'c-mode-hook 'whitespace-mode)
-  (add-hook 'c++-mode-hook 'whitespace-mode)
+  ;; (add-hook 'c-mode-hook 'whitespace-mode)
+  ;; (add-hook 'c++-mode-hook 'whitespace-mode)
   ;;; whitespace mode setting end
 
   ;;; org setting begin
@@ -530,14 +534,15 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol t)
  '(package-selected-packages
    (quote
-    (org-brain youdao-dictionary yasnippet-snippets yapfify wttrin ws-butler winum which-key volatile-highlights vi-tilde-fringe vala-snippets vala-mode uuidgen use-package unfill toc-org thrift symon switch-window string-inflection stan-mode spaceline-all-the-icons smeargle shell-pop scad-mode restart-emacs realgud rainbow-delimiters qml-mode pyvenv pytest pyenv-mode py-isort popwin pkgbuild-mode pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets open-junk-file neotree nameless mwim multi-term move-text mmm-mode matlab-mode markdown-toc magit-gitflow magit-gh-pulls macrostep lorem-ipsum logcat live-py-mode linum-relative link-hint kivy-mode keyfreq julia-mode insert-shebang indent-guide importmagic ibuffer-projectile hy-mode hungry-delete htmlize hoon-mode hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-rtags helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-flx helm-descbinds helm-cscope helm-company helm-c-yasnippet helm-ag google-translate google-c-style golden-ratio gnuplot github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md ggtags fuzzy font-lock+ flyspell-correct-helm flycheck-ycmd flycheck-rtags flycheck-pos-tip flycheck-bashate flx-ido fish-mode fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks elisp-slime-nav editorconfig ebuild-mode dumb-jump dockerfile-mode docker disaster diminish diff-hl define-word cython-mode counsel-projectile company-ycmd company-statistics company-shell company-rtags company-quickhelp company-c-headers company-anaconda column-enforce-mode clean-aindent-mode clang-format centered-cursor-mode browse-at-remote auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile arduino-mode aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
+    (flucui-themes magit-gh-pulls youdao-dictionary yasnippet-snippets yapfify wttrin ws-butler writeroom-mode wolfram-mode winum which-key volatile-highlights vi-tilde-fringe vala-snippets vala-mode uuidgen use-package unfill treemacs-projectile toc-org thrift symon switch-window sublimity string-inflection stan-mode spaceline-all-the-icons smeargle shell-pop scad-mode restart-emacs rainbow-delimiters qml-mode pyvenv pytest pyenv-mode py-isort popwin pkgbuild-mode pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file nameless mwim multi-term move-text mmm-mode matlab-mode markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum logcat live-py-mode linum-relative link-hint kivy-mode keyfreq insert-shebang indent-guide importmagic ibuffer-projectile hungry-delete htmlize hoon-mode hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-rtags helm-pydoc helm-purpose helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gtags helm-gitignore helm-git-grep helm-flx helm-descbinds helm-cscope helm-company helm-c-yasnippet helm-ag google-translate google-c-style golden-ratio gnuplot gitignore-templates github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md ggtags fuzzy forge font-lock+ flyspell-correct-helm flycheck-ycmd flycheck-rtags flycheck-pos-tip flycheck-bashate flx-ido fish-mode fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-org evil-numbers evil-nerd-commenter evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks elisp-slime-nav editorconfig ebuild-mode dumb-jump dotenv-mode doom-themes doom-modeline dockerfile-mode docker disaster diminish diff-hl define-word cython-mode counsel-projectile company-ycmd company-statistics company-shell company-rtags company-quickhelp company-c-headers company-anaconda column-enforce-mode clean-aindent-mode clang-format centered-cursor-mode browse-at-remote auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile arduino-mode aggressive-indent ace-link ace-jump-helm-line ac-ispell)))
+ '(paradox-automatically-star t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "DarkGoldenrod2"))))
- '(spacemacs-emacs-face ((t (:foreground "black" :background "DarkGoldenrod2")))))
+ )
 )
